b1164c8c978f3b4dd7665b91b990f2daeb81e21c
Alexnarea <lanarea@sudamericano.edu.ec>
2025-06-29 21:40:03 -0500

    update-end

--- evaluations-ms/evaluations-ms/src/main/kotlin/com/example/evaluations_ms/controller/EvaluationsController.kt
+++ evaluations-ms/evaluations-ms/src/main/kotlin/com/example/evaluations_ms/controller/EvaluationsController.kt
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/evaluations")
+@RequestMapping("/evaluations")
 class EvaluationsController {
 
     @Autowired
